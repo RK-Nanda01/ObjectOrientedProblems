95e6cd1db7fb3068928caffb753f43524874f941
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-25 10:49:11 +0530

    [RK Nanda] Adding Method to find duplicate elements

---
+++ ObjectOrientedProblems/ArrayManipulation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace ObjectOrientedProblems
+{
+    public class ArrayManipulation
+    {
+        public int [] initialiseArray(int n)
+        {
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements of the array");
+            for(int i = 0;i<n; i++)
+            {
+               arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            return arr;
+        }
+		public int DuplicateCount(int[] arr)
+		{
+            Dictionary<int, int> dupCount = new Dictionary<int, int>();
+
+           
+            foreach (int num in arr)
+            {
+                if (dupCount.ContainsKey(num))
+                    dupCount[num]++;
+                else
+                   dupCount[num] = 1;
+            }
+
+            
+            int totalDuplicates = 0;
+            foreach (var pair in dupCount)
+            {
+                if (pair.Value > 1)
+                    totalDuplicates++;
+            }
+
+            return totalDuplicates;
+        }
+    }
+	
+}
+
--- ObjectOrientedProblems/Program.cs
+++ ObjectOrientedProblems/Program.cs
@@ -3,8 +3,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        bool flag = true;
+        int option;
         Console.WriteLine("Welcome to Object Oriented Problems");
-        Console.ReadLine();
+        while (flag)
+        {
+            Console.WriteLine("MENU:->");
+            Console.WriteLine("1. Find total Duplicates in an array");
+            Console.WriteLine("0. Exit");
+            option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 0:
+                    {
+                        flag = false;
+                        break;
+                    }
+                case 1:
+                    {
+                        int n;
+                        Console.WriteLine("Enter the size of the array");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        ArrayManipulation a = new ArrayManipulation();
+                        int[] arr = a.initialiseArray(n);
+                        int dup = a.DuplicateCount(arr);
+                        Console.WriteLine($"Count of Duplicate Elements is {dup}");
+                        break;
+                    }
+            }
+        }
     }
 
 }
